81d446bcd7023ae1470ee52473dc32c813c7c250
19wh1a0579 <69663926+19wh1a0579@users.noreply.github.com>
2026-05-31 15:55:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from huggingface_hub import hf_hub_download
 
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.resnet_v2 import preprocess_input
+from tensorflow.keras.applications.resnet_v2 
+import preprocess_input
 
 
 st.set_page_config(
