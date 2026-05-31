b0c14c2d68f4b6e00092d48739fe52ae93900972
19wh1a0579 <69663926+19wh1a0579@users.noreply.github.com>
2026-05-31 15:54:18 +0530

    Update app.py
    Added model loading from Hugging Face

--- app.py
+++ app.py
@@ -24,3 +24,43 @@ st.title(
 st.write(
     "Prediction + Confidence + Uncertainty + Grad-CAM"
 )
+
+
+
+@st.cache_resource
+def load_brain_tumor_model():
+
+    model_path = hf_hub_download(
+        repo_id="suvidha-reddy/brain-tumor-trustworthy-ai",
+        filename="brain_tumor_resnet152v2.h5"
+    )
+
+    model = load_model(model_path)
+
+    return model
+
+loaded_model = load_brain_tumor_model()
+
+st.success("Model Loaded Successfully")
+
+
+
+class_names = [
+    "glioma",
+    "meningioma",
+    "notumor",
+    "pituitary"
+]
+
+
+
+resnet_model = loaded_model.get_layer("resnet152v2")
+
+grad_backbone = tf.keras.Model(
+    inputs=resnet_model.input,
+    outputs=resnet_model.get_layer("post_relu").output
+)
+
+
+
+
